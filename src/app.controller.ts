07f99cad0dc30709aeb6ea10b1891ca2cb41d2d6
Clara <ferreira.clara22@hotmail.com>
2022-08-17 17:40:19 -0300

    puxando dados api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('BRAandUSA')
+  getDatasBU() {
+    return this.appService.getDatasBU();
+  }
+
+  @Get('RUSandCHN')
+  getDatasRC() {
+    return this.appService.getDatasRC();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import fetch from 'cross-fetch';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getDatasBU() {
+    const response = await fetch ('https://disease.sh/v3/covid-19/countries/brazil%2C%20usa')
+    const data = await response.json()
+    return data
+  }
+
+  async getDatasRC() {
+    const response = await fetch ('https://disease.sh/v3/covid-19/countries/russia%2C%20china')
+    const data = await response.json()
+    return data
   }
 }
