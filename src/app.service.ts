26246ed886e0b5d76709a7e64349a0b8cf75e79e
Clara <ferreira.clara22@hotmail.com>
2022-08-17 19:35:19 -0300

    objeto com dados necessários

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,20 @@ export class AppService {
   async getDatasBU() {
     const response = await fetch ('https://disease.sh/v3/covid-19/countries/brazil%2C%20usa')
     const data = await response.json()
+
+    const date = new Date();
+    const today = date.toLocaleDateString();
+
+    const datas = data.map((item) => ({
+      country: item.country,
+      date: today,
+      todayCases: item.todayCases,
+      todayDeaths: item.todayDeaths,
+      active: item.active,
+      critical: item.critical
+    }))
+
+    console.log(datas)
     return data
   }
 
